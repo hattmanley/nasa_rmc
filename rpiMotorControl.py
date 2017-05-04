3f85d0d55baf01edd390cff89a80a88d5ac89dd8
Matthew Hanley <mattdhanley@gmail.com>
2017-05-04 15:37:54 -0600

    working on it

--- rpiMotorControl.py
+++ rpiMotorControl.py
@@ -1,24 +1,42 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import rospy
+
+def callback(data):
+	rospy.loginfo(rospy.get_caller_id() + "Recieved data %s",data.data)
+
+def motorControl():
+	rospy.init_node('motorControl',anoymous=True)
+	rospy.Subscriber('joy_node',buttons,callback)
+	print "Buttons: "
+	print buttons
+
+	rospy.spin()
+
+if __name__ == '__main__':
+	motorControl()
+
+
+
+
+# GPIO.setmode(GPIO.BOARD)
  
-GPIO.setmode(GPIO.BOARD)
+# Motor1A = 16
+# Motor1B = 18
+# Motor1E = 22
  
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
+# GPIO.setup(Motor1A,GPIO.OUT)
+# GPIO.setup(Motor1B,GPIO.OUT)
+# GPIO.setup(Motor1E,GPIO.OUT)
  
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
+# print "Turning motor on"
+# GPIO.output(Motor1A,GPIO.HIGH)
+# GPIO.output(Motor1B,GPIO.LOW)
+# GPIO.output(Motor1E,GPIO.HIGH)
  
-print "Turning motor on"
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
-GPIO.output(Motor1E,GPIO.HIGH)
+# sleep(2)
  
-sleep(2)
+# print "Stopping motor"
+# GPIO.output(Motor1E,GPIO.LOW)
  
-print "Stopping motor"
-GPIO.output(Motor1E,GPIO.LOW)
- 
-GPIO.cleanup()
+# GPIO.cleanup()
